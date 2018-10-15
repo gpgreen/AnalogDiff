ab2f8cbba2523cc5449a6929c4bd31d7174b2137
Greg Green <gpgreen@gmail.com>
2018-10-15 09:27:42 -0700

    Added gplv3 license
    Updated README and library metadata
    Changed headers on source code files

--- src/AnalogDiff.cpp
+++ src/AnalogDiff.cpp
@@ -1,26 +1,10 @@
-/*
-  AnalogDiff.cpp - Using ADC differential conversion hardware
-  Greg Green <gpgreen@gmail.com>
-
-  History:
-	23/July/18 - V0.1 released working on ATMEGA644 only
-	
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+//============================================================================
+// Name        : AnalogDiff.cpp
+// Author      : Greg Green <gpgreen@gmail.com>
+// Version     : 1.0
+// Copyright   : GPL v3
+// Description : Read differential channel on ADC hardware
+//=======================================================================
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
